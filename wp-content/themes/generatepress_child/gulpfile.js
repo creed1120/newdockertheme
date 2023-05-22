00e57b8e65fec80cb69d46a02f3b7131dcd9481f
creed1120 <cedricareed@gmail.com>
2023-05-22 00:38:21 -0700

    added bs js

--- wp-content/themes/generatepress_child/gulpfile.js
+++ wp-content/themes/generatepress_child/gulpfile.js
@@ -2,9 +2,14 @@
 
 // const gulp = require('gulp');
 
-const { src, dest } = require("gulp");
+const { src, dest, watch, parallel } = require("gulp");
 const sass = require('gulp-sass')(require('sass'));
 const webpack = require('webpack-stream');
+
+// const filePaths = {
+//     js: './assets/src/js/**/*.js',
+//     css: './assets/src/scss/**/*.scss'
+// }
 
 function buildStyles(cb) {
     src('./assets/src/scss/**/*.scss')
@@ -25,5 +30,12 @@ function buildScripts(cb) {
     cb();
 }
 
+function watchFiles(cb) {
+    watch(['./assets/src/js/**/*.js', './assets/src/scss/**/*.scss'], parallel(buildStyles, buildScripts))
+    cb();
+}
+
 exports.buildStyles = buildStyles;
 exports.buildScripts = buildScripts;
+
+exports.watch = watchFiles;
